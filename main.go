21eba8371bd2f83d48d2128d3667931713e04c56
sundh4 <kd.suryadharma@gmail.com>
2022-06-12 21:58:18 +0800

    Add unit test

--- main.go
+++ main.go
@@ -8,8 +8,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func HomepageHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", gin.H{
+		"message": "This is an main page...",
+	})
+}
+
 func main() {
 	router := gin.Default()
+
 	router.SetFuncMap(template.FuncMap{
 		"upper": strings.ToUpper,
 	})
@@ -17,11 +24,7 @@ func main() {
 	router.Static("/assets", "./assets")
 	router.LoadHTMLGlob("templates/*.html")
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"content": "This is an index page...",
-		})
-	})
+	router.GET("/", HomepageHandler)
 
 	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
